d04237fbcf30e1d18cad5cc056ccfc375e0aa557
Roberto <robertomartinonwg@gmail.com>
2021-09-09 19:03:49 +0200

    nextPhotos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
+
+
 let app = new Vue({
     el: '#root',
     data: {
+        counter: 0,
         title: "Guarda le slide",
-        image: "img/image1.jpg"
+        images: ["img/image1.jpg","img/image2.jpg", "img/image3.jpg", "img/image4.jpg", "img/diego-armando-maradona.jpg"]
+    },  
+    methods: {
+        nextPhotos(){
+            this.counter += 1
+            if(this.counter > (this.images.length -1)) {
+                this.counter = 0
+            }
+        }
     }
-    
 })
